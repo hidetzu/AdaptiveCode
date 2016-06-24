815a2690d2ab63c424f3ac55d234838349d07923
hidetzu <hidetzu@gmail.com>
2016-06-25 06:29:11 +0900

    インターフェイス定義を追加

---
+++ Chapter3/ch3-interface-definition/TheInterface/ISimpleInterface.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    public interface ISimpleInterface
+    {
+        void ThisMethodRequiresImplement();
+
+        string ThisStringPropertyNeedsImplementingToo
+        {
+            set;
+            get;
+        }
+
+        int ThisIntPropertyOnlyNeedsAGetter
+        {
+            get;
+        }
+
+        event EventHandler<EventArgs> InterfaceCanContainEventsToo;
+    }
+}
