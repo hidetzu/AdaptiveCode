7c03dd1b5ed7cc5fca38362a2e324fed61899724
hidetzu <hidetzu@gmail.com>
2016-06-25 06:44:36 +0900

    複数のインターフェイスを実装する例

---
+++ Chapter3/ch3-interface-definition/TheInterface/ImplementingMultipleInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    public class ImplementingMultipleInterface : InterfaceOne, InterfaceTwo
+    {
+        public void MethodOne()
+        {
+        }
+
+        public void MethodTwo()
+        {
+        }
+    }
+}
---
+++ Chapter3/ch3-interface-definition/TheInterface/SimpleInterfaceImplementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheInterface;
+
+namespace TheInterface
+{
+    public class SimpleInterfaceImplementation : ISimpleInterface
+    {
+        private int encapsulatedInteger;
+        public int ThisIntPropertyOnlyNeedsAGetter
+        {
+            get
+            {
+                return this.encapsulatedInteger;
+            }
+
+            set
+            {
+                this.encapsulatedInteger = value;
+            }
+        }
+
+        public string ThisStringPropertyNeedsImplementingToo
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public event EventHandler<EventArgs> InterfaceCanContainEventsToo = delegate { };
+
+        public void ThisMethodRequiresImplement()
+        {
+        }
+    }
+}
