7522da76d8070592ced259f6c0a45570f2557341
hidetzu <hidetzu@gmail.com>
2016-06-25 19:09:53 +0900

    Adapterパターンのサンプル

---
+++ Chapter3/ch3-adapter-pattern/TheInterface/ObjectAdapterPatternSample/Adapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectAdapterPatternSample
+{
+    public class Adapter : IExpectedInterface
+    {
+        private TargetClass target;
+
+        public Adapter(TargetClass traget)
+        {
+            this.target = traget;
+        }
+
+        public void MethodA()
+        {
+            target.MethodB();
+        }
+    }
+}
---
+++ Chapter3/ch3-adapter-pattern/TheInterface/TheInterface/Program.cs
@@ -0,0 +1,22 @@
+﻿using AdapterPatternSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    class Program
+    {
+        static Adapter dependency = new Adapter();
+        static ObjectAdapterPatternSample.Adapter dependency2 = new ObjectAdapterPatternSample.Adapter(
+            new ObjectAdapterPatternSample.TargetClass());
+
+        static void Main(string[] args)
+        {
+            dependency.MethodA();
+            dependency2.MethodA();
+        }
+    }
+}
