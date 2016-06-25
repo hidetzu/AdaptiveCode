50d168ed3ad5b70a9217cfab55ef02cdbc9decb4
hidetzu <hidetzu@gmail.com>
2016-06-25 21:31:29 +0900

    動的型付け

---
+++ Chapter3/ch3-duck-typing-using-dlr/TheInterface/TheInterface/Program.cs
@@ -0,0 +1,29 @@
+﻿using DuckTyping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var swan = new Swan();
+            DoDuckLikeThings(swan);
+            Console.ReadLine();
+        }
+
+        static void DoDuckLikeThings(dynamic duckish)
+        {
+            if(duckish != null)
+            {
+                duckish.Walk();
+                duckish.Swim();
+                duckish.Quack();
+            }
+        }
+    }
+}
