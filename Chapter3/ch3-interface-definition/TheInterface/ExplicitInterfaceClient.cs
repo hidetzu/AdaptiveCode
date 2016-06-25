90ffc33c1db0caa344857fd1a68cff41ceba6b2f
hidetzu <hidetzu@gmail.com>
2016-06-25 09:39:00 +0900

    sampleを追加

---
+++ Chapter3/ch3-interface-definition/TheInterface/ExplicitInterfaceClient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    public class ExplicitInterfaceClient
+    {
+        public ExplicitInterfaceClient(ExplicitInterfaceImplementation implementationReference, ISimpleInterface interfaceReference)
+        {
+            var instancePropertyValue = interfaceReference.ThisIntPropertyOnlyNeedsAGetter;
+            interfaceReference.ThisMethodRequiresImplement();
+            interfaceReference.ThisStringPropertyNeedsImplementingToo = "hello";
+            interfaceReference.InterfaceCanContainEventsToo += EventHandler;
+        }
+
+        void EventHandler(object sender, EventArgs e)
+        {
+        }
+    }
+}
---
+++ Chapter3/ch3-interface-definition/TheInterface/ExplicitInterfaceImplementation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    public class ExplicitInterfaceImplementation : ISimpleInterface
+    {
+        private EventHandler<EventArgs> encapsulatedEvent;
+        private int encapsulatedInteger;
+
+        public ExplicitInterfaceImplementation()
+        {
+            this.encapsulatedInteger = 4;
+        }
+
+        public object ThisIntPropertyOnlyNeedsAGetter { get; internal set; }
+
+        int ISimpleInterface.ThisIntPropertyOnlyNeedsAGetter
+        {
+            get
+            {
+                return this.encapsulatedInteger;
+            }
+        }
+
+        string ISimpleInterface.ThisStringPropertyNeedsImplementingToo
+        {
+            get;
+            set;
+        }
+
+        event EventHandler<EventArgs> ISimpleInterface.InterfaceCanContainEventsToo
+        {
+            add { encapsulatedEvent += value; } 
+            remove { encapsulatedEvent -= value; }
+        }
+
+        void ISimpleInterface.ThisMethodRequiresImplement()
+        {
+        }
+    }
+}
