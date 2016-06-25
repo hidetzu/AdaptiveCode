fce18b4022174f6e199c437f1a3a509d522e1477
hidetzu <hidetzu@gmail.com>
2016-06-25 21:15:11 +0900

    strategyパターン

---
+++ Chapter3/ch3-strategy-pattern/TheInterface/TheInterface/Context.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    public class Context
+    {
+        public Context()
+        {
+            currentStrategy = strategyA;
+        }
+
+        public void DoSomething()
+        {
+            currentStrategy.Execute();
+
+            currentStrategy = (currentStrategy == strategyA) ? strategyB : strategyA;
+        }
+
+        private readonly IStrategy strategyA = new ConcreteStrategyA();
+        private readonly IStrategy strategyB = new ConcreteStrategyB();
+
+        private IStrategy currentStrategy;
+    }
+}
