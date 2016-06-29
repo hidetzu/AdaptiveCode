8711ca2432a5767700ff69aec3ec8df7973bfa44
hidetzu <hidetzu@gmail.com>
2016-06-29 21:20:16 +0900

    ch4 サンプル

---
+++ Chapter4/ch4-arrange-the-preconditions/UnitTesting/UnitTesting/AccountTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomerAccounts;
+
+namespace UnitTesting
+{
+    [TestClass]
+    public class AccountTest
+    {
+        [TestMethod]
+        public void AddingTransactionChangeBalance()
+        {
+            var account = new Account();
+
+            account.AddTransaction(200m);
+
+            Assert.AreEqual(200m, account.Balance);
+        }
+    }
+}
