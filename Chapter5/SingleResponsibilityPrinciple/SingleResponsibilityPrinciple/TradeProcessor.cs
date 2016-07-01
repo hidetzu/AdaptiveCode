6b93dc7c97c47d0598b9337c3f1cbd3d57fbc085
hidetzu <hidetzu@gmail.com>
2016-07-02 00:19:44 +0900

    コードを明確にするためのリファクタリング

--- Chapter5/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/TradeProcessor.cs
+++ Chapter5/SingleResponsibilityPrinciple/SingleResponsibilityPrinciple/TradeProcessor.cs
@@ -9,67 +9,16 @@ namespace SingleResponsibilityPrinciple
 {
     public class TradeProcessor
     {
+
         public void ProcessTrades(Stream stream)
         {
-            // read rows
-            var lines = new List<string>();
-            using (var reader = new StreamReader(stream))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    lines.Add(line);
-                }
-            }
+            var lines = ReadTradeData(stream);
+            var trades = ParseTrades(lines);
+            StoreTrades(trades);
+        }
 
-            var trades = new List<TradeRecord>();
-
-            var lineCount = 1;
-            foreach (var line in lines)
-            {
-                var fields = line.Split(new char[] { ',' });
-
-                if (fields.Length != 3)
-                {
-                    Console.WriteLine("WARN: Line {0} malformed. Only {1} field(s) found.", lineCount, fields.Length);
-                    continue;
-                }
-
-                if (fields[0].Length != 6)
-                {
-                    Console.WriteLine("WARN: Trade currencies on line {0} malformed: '{1}'", lineCount, fields[0]);
-                    continue;
-                }
-
-                int tradeAmount;
-                if (!int.TryParse(fields[1], out tradeAmount))
-                {
-                    Console.WriteLine("WARN: Trade amount on line {0} not a valid integer: '{1}'", lineCount, fields[1]);
-                }
-
-                decimal tradePrice;
-                if (!decimal.TryParse(fields[2], out tradePrice))
-                {
-                    Console.WriteLine("WARN: Trade price on line {0} not a valid decimal: '{1}'", lineCount, fields[2]);
-                }
-
-                var sourceCurrencyCode = fields[0].Substring(0, 3);
-                var destinationCurrencyCode = fields[0].Substring(3, 3);
-
-                // calculate values
-                var trade = new TradeRecord
-                {
-                    SourceCurrency = sourceCurrencyCode,
-                    DestinationCurrency = destinationCurrencyCode,
-                    Lots = tradeAmount / LotSize,
-                    Price = tradePrice
-                };
-
-                trades.Add(trade);
-
-                lineCount++;
-            }
-
+        private static void StoreTrades(IEnumerable<TradeRecord> trades)
+        {
             using (var connection = new System.Data.SqlClient.SqlConnection("Data Source=.\\SQLEXPRESS;Initial Catalog=TradeDatabase;Integrated Security=True;"))
             {
                 connection.Open();
@@ -94,7 +43,99 @@ namespace SingleResponsibilityPrinciple
                 connection.Close();
             }
 
-            Console.WriteLine("INFO: {0} trades processed", trades.Count);
+            Console.WriteLine("INFO: {0} trades processed", trades.Count());
+        }
+
+        private IEnumerable<TradeRecord> ParseTrades(IEnumerable<string> lines)
+        {
+            var trades = new List<TradeRecord>();
+
+            var lineCount = 1;
+            foreach (var line in lines)
+            {
+                var fields = line.Split(new char[] { ',' });
+
+                if (!ValidateTradeData(fields, lineCount))
+                    continue;
+
+                TradeRecord trade = MapTradeDataToTradeRecord(fields);
+
+                trades.Add(trade);
+
+                lineCount++;
+            }
+
+            return trades;
+        }
+
+        private TradeRecord MapTradeDataToTradeRecord(string[] fields)
+        {
+            var sourceCurrencyCode = fields[0].Substring(0, 3);
+            var destinationCurrencyCode = fields[0].Substring(3, 3);
+            var tradeAmount = int.Parse(fields[1]);
+            var tradePrice = decimal.Parse(fields[2]);
+
+            // calculate values
+            var trade = new TradeRecord
+            {
+                SourceCurrency = sourceCurrencyCode,
+                DestinationCurrency = destinationCurrencyCode,
+                Lots = tradeAmount / LotSize,
+                Price = tradePrice
+            };
+            return trade;
+        }
+
+        private bool ValidateTradeData(string[] fields, int currentLine)
+        {
+            if (fields.Length != 3)
+            {
+                LogMessage("WARN: Line {0} malformed. Only {1} field(s) found.", currentLine, fields.Length);
+                return false;
+            }
+
+            if (fields[0].Length != 6)
+            {
+                LogMessage("WARN: Trade currencies on line {0} malformed: '{1}'", currentLine, fields[0]);
+                return false;
+            }
+
+            int tradeAmount;
+            if (!int.TryParse(fields[1], out tradeAmount))
+            {
+                LogMessage("WARN: Trade amount on line {0} not a valid integer: '{1}'", currentLine, fields[1]);
+                return false;
+            }
+
+            decimal tradePrice;
+            if (!decimal.TryParse(fields[2], out tradePrice))
+            {
+                LogMessage("WARN: Trade price on line {0} not a valid decimal: '{1}'", currentLine, fields[2]);
+                return false;
+            }
+
+            return true;
+        }
+
+        private IEnumerable<string> ReadTradeData(Stream stream)
+        {
+            // read rows
+            var lines = new List<string>();
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+
+            return lines;
+        }
+
+        private void LogMessage(string message, params object[] args)
+        {
+            Console.WriteLine(message, args);
         }
 
         private static float LotSize = 100000f;
