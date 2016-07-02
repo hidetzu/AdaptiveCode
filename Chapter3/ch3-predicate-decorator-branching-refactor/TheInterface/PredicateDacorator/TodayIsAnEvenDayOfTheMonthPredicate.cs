3acd2554055802a98c779415afe349ef0c187f10
hidetzu <hidetzu@gmail.com>
2016-07-02 21:07:51 +0900

    分岐デコレーター

---
+++ Chapter3/ch3-predicate-decorator-branching-refactor/TheInterface/PredicateDacorator/TodayIsAnEvenDayOfTheMonthPredicate.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateDecorate
+{
+    public class TodayIsAnEvenDayOfTheMonthPredicate : IPredicate
+    {
+        private readonly DataTester dataTester;
+
+        public TodayIsAnEvenDayOfTheMonthPredicate(DataTester dataTester)
+        {
+            this.dataTester = dataTester;
+        }
+
+        public bool Test()
+        {
+            return dataTester.TodayIsAnEvenDayOfTheMonth;
+        }
+    }
+}
