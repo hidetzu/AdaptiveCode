52e17c4e351cb336793bb53aba5193c90d98f670
hidetzu <hidetzu@gmail.com>
2016-07-02 17:48:57 +0900

    compositeパターン

---
+++ Chapter3/ch3-composite-pattern/TheInterface/CompositePattern/CompositeComponent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositePattern
+{
+    public class CompositeComponent : IComponent
+    {
+        private ICollection<IComponent> children;
+
+        public CompositeComponent()
+        {
+            children = new List<IComponent>();
+        }
+
+        public void Add(IComponent component)
+        {
+            children.Add(component);
+        }
+
+        public void Remove(IComponent component)
+        {
+            children.Remove(component);
+        }
+
+
+        public void Something()
+        {
+            foreach(var child in children)
+            {
+                child.Something();
+            }
+        }
+    }
+}
---
+++ Chapter3/ch3-composite-pattern/TheInterface/TheInterface/Program.cs
@@ -0,0 +1,24 @@
+﻿using CompositePattern;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheInterface
+{
+    class Program
+    {
+        static IComponent component;
+        static void Main(string[] args)
+        {
+            var composite = new CompositeComponent();
+            composite.Add(new Leaf());
+            composite.Add(new SecondTypeOfLeaf());
+            composite.Add(new AThirdLeafType());
+
+            component = composite;
+            component.Something();
+        }
+    }
+}
