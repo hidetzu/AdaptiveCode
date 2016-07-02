3533436c0ddeb3ad196b202eca6948d265ffafdc
hidetzu <hidetzu@gmail.com>
2016-07-02 18:25:57 +0900

    述語デコレーター

---
+++ Chapter3/ch3-predicate-decorator/TheInterface/PredicateDecorate/PredicatedComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateDecorate
+{
+    public class PredicatedComponent : IComponent
+    {
+        private readonly IComponent component;
+        private readonly IPredicate predicate;
+
+        public PredicatedComponent(IComponent component, IPredicate predicate)
+        {
+            this.component = component;
+            this.predicate = predicate;
+        }
+
+
+        public void Something()
+        {
+            if (predicate.Test())
+            {
+                component.Something();
+
+            }
+        }
+    }
+}
