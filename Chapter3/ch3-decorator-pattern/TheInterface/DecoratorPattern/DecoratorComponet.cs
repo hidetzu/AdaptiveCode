8f55bfac4870a510e6185d6335f5f8c66edee909
hidetzu <hidetzu@gmail.com>
2016-07-02 17:33:29 +0900

    decoratorパターン

---
+++ Chapter3/ch3-decorator-pattern/TheInterface/DecoratorPattern/DecoratorComponet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorPattern
+{
+    public class DecoratorComponet : IComponent
+    {
+        private readonly IComponent decoratedComponet;
+
+        public DecoratorComponet(IComponent decoratedComponet)
+        {
+            this.decoratedComponet = decoratedComponet;
+        }
+
+        public void Something()
+        {
+            SomethingElse();
+            this.decoratedComponet.Something();
+        }
+
+        private void SomethingElse()
+        {
+        }
+    }
+}
---
+++ Chapter3/ch3-decorator-pattern/TheInterface/TheInterface/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DecoratorPattern;
+
+namespace TheInterface
+{
+    class Program
+    {
+        static IComponent component;
+        static void Main(string[] args)
+        {
+            component = new DecoratorComponet(new ConcreteComponet());
+            component.Something();
+        }
+    }
+}
