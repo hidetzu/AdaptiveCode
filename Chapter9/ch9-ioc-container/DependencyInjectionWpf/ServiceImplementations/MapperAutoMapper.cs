1b8b3afe84703276aaff021dea05b6333dfb764d
hidetzu <hidetzu@gmail.com>
2016-07-21 00:49:06 +0900

    UnityContainerを使ってみる。

---
+++ Chapter9/ch9-ioc-container/DependencyInjectionWpf/ServiceImplementations/MapperAutoMapper.cs
@@ -0,0 +1,18 @@
+﻿using ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceImplementations
+{
+    public class MapperAutoMapper : IObjectMapper
+    {
+
+        public TDestination Map<TDestination>(object source)
+        {
+            return AutoMapper.Mapper.Map<TDestination>(source);
+        }
+    }
+}
---
+++ Chapter9/ch9-ioc-container/DependencyInjectionWpf/UI/App.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Controllers;
+using Microsoft.Practices.Unity;
+using ServiceImplementations;
+using ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using UI;
+using ViewModels;
+
+namespace DependencyInjectionWpf
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void OnApplicationStartup(object sender, StartupEventArgs e)
+        {
+            CreateMappings();
+
+            container = new UnityContainer();
+            container.RegisterType<ISettings, ApplicationSettings>();
+            container.RegisterType<ITaskService, TaskServiceAdo>();
+            container.RegisterType<IObjectMapper, MapperAutoMapper>();
+            container.RegisterType<TaskListController>();
+            container.RegisterType<TaskListView>();
+
+            MainWindow = container.Resolve<TaskListView>();
+            MainWindow.Show();
+
+            ((TaskListController)MainWindow.DataContext).OnLoad();
+        }
+
+        private void CreateMappings()
+        {
+            AutoMapper.Mapper.CreateMap<TaskDto, TaskViewModel>();
+        }
+
+        private IUnityContainer container;
+    }
+}
