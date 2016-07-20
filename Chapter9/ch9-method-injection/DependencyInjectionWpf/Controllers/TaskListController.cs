118c56ad1dce1dfa372e43de26aa32dc2d8ad162
hidetzu <hidetzu@gmail.com>
2016-07-21 00:25:19 +0900

    メソッド注入

---
+++ Chapter9/ch9-method-injection/DependencyInjectionWpf/Controllers/TaskListController.cs
@@ -0,0 +1,51 @@
+﻿using ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViewModels;
+
+namespace Controllers
+{
+    public class TaskListController : INotifyPropertyChanged
+    {
+        public TaskListController(ITaskService taskService, IObjectMapper mapper, ISettings settings)
+        {
+            this.taskService = taskService;
+            this.mapper = mapper;
+            this.settings = settings;
+        }
+
+        public ObservableCollection<TaskViewModel> AllTasks
+        {
+            get
+            {
+                return allTasks;
+            }
+            set
+            {
+                allTasks = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("AllTasks"));
+            }
+        }
+
+        public void OnLoad()
+        {
+            var taskDtos = taskService.GetAllTasks(settings);
+            AllTasks = new ObservableCollection<TaskViewModel>(mapper.Map<IEnumerable<TaskViewModel>>(taskDtos));
+        }
+
+
+        private readonly ITaskService taskService;
+        private readonly IObjectMapper mapper;
+        private readonly ISettings settings;
+
+        private ObservableCollection<TaskViewModel> allTasks;
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+
+}
